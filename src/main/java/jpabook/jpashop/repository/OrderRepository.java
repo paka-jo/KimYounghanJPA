0cc053439a83194a5337422d80e71f98d806c71a
paka <wjd1357912@gmail.com>
2025-05-16 11:32:22 +0900

    simpleOrder refactor (fetch join, direct query of dto in jpa)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -20,7 +21,7 @@ public class OrderRepository {
     }
 
     public Order findOne(Long id) {
-        return em.find(Order.class , id);
+        return em.find(Order.class, id);
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
@@ -32,8 +33,8 @@ public class OrderRepository {
             if (isFirstCondition) {
                 jpql += " where";
                 isFirstCondition = false;
-            }
-            else {jpql += " and";
+            } else {
+                jpql += " and";
             }
             jpql += " o.status = :status";
         }
@@ -42,8 +43,8 @@ public class OrderRepository {
             if (isFirstCondition) {
                 jpql += " where";
                 isFirstCondition = false;
-            }
-            else { jpql+= " and";
+            } else {
+                jpql += " and";
             }
             jpql += " m.name like :name";
         }
@@ -57,4 +58,13 @@ public class OrderRepository {
         }
         return query.getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
+
 }
